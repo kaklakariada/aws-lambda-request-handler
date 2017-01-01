b541895fa5d3f8d6cee6cc904808df4f159bd5fa
kaklakariada <kaklakariada@users.noreply.github.com>
2017-01-01 11:52:57 +0100

    Update year to 2017

--- src/main/java/com/github/kaklakariada/aws/lambda/ApiGatewayResponse.java
+++ src/main/java/com/github/kaklakariada/aws/lambda/ApiGatewayResponse.java
@@ -1,6 +1,6 @@
 /**
  * aws-lambda-request-handler - Request handler for AWS Lambda Proxy model
- * Copyright (C) 2015 Christoph Pirkl <christoph at users.sourceforge.net>
+ * Copyright (C) 2017 Christoph Pirkl <christoph at users.sourceforge.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
