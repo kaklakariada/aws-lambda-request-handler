eb1e76f9d20fd2a530666b8a4bdee2992717b2dc
kaklakariada <kaklakariada@users.noreply.github.com>
2017-05-14 16:00:55 +0200

    Log and return all input

--- test-project/src/main/java/handler/echo/EchoHandler.java
+++ test-project/src/main/java/handler/echo/EchoHandler.java
@@ -1,10 +1,18 @@
 package handler.echo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.lambda.runtime.Context;
 import com.github.kaklakariada.aws.lambda.LambdaRequestHandler;
 import com.github.kaklakariada.aws.lambda.controller.LambdaController;
+import com.github.kaklakariada.aws.lambda.controller.RequestBody;
 import com.github.kaklakariada.aws.lambda.controller.RequestHandlerMethod;
+import com.github.kaklakariada.aws.lambda.request.ApiGatewayRequest;
 
 public class EchoHandler extends LambdaRequestHandler<EchoRequest, EchoResponse> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EchoHandler.class);
 
     public EchoHandler() {
         super(new EchoController(), EchoRequest.class, EchoResponse.class);
@@ -12,8 +20,11 @@ public class EchoHandler extends LambdaRequestHandler<EchoRequest, EchoResponse>
 
     public static class EchoController implements LambdaController<EchoRequest, EchoResponse> {
         @RequestHandlerMethod
-        public EchoResponse handleRequest() {
-            return new EchoResponse("empty");
+        public EchoResponse handleRequest(@RequestBody EchoRequest body, Context context, ApiGatewayRequest request) {
+            LOG.info("Request body: {}", body);
+            LOG.info("Context     : {}", context);
+            LOG.info("Request     : {}", request);
+            return new EchoResponse("empty", body, request);
         }
     }
 }
--- test-project/src/main/java/handler/echo/EchoResponse.java
+++ test-project/src/main/java/handler/echo/EchoResponse.java
@@ -1,13 +1,27 @@
 package handler.echo;
 
+import com.github.kaklakariada.aws.lambda.request.ApiGatewayRequest;
+
 public class EchoResponse {
     private final String message;
+    private final ApiGatewayRequest request;
+    private final EchoRequest body;
 
-    public EchoResponse(String message) {
+    public EchoResponse(String message, EchoRequest body, ApiGatewayRequest request) {
         this.message = message;
+        this.body = body;
+        this.request = request;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public EchoRequest getBody() {
+        return body;
+    }
+
+    public ApiGatewayRequest getRequest() {
+        return request;
     }
 }
