1fc1361f0bf806e6d810a361f7026352b99e0c47
kaklakariada <kaklakariada@users.noreply.github.com>
2017-05-14 15:51:27 +0200

    Add response

--- test-project/src/main/java/handler/echo/EchoHandler.java
+++ test-project/src/main/java/handler/echo/EchoHandler.java
@@ -2,14 +2,18 @@ package handler.echo;
 
 import com.github.kaklakariada.aws.lambda.LambdaRequestHandler;
 import com.github.kaklakariada.aws.lambda.controller.LambdaController;
+import com.github.kaklakariada.aws.lambda.controller.RequestHandlerMethod;
 
-public class EchoHandler extends LambdaRequestHandler<EchoRequest,EchoResponse>{
+public class EchoHandler extends LambdaRequestHandler<EchoRequest, EchoResponse> {
 
-	protected EchoHandler() {
-		super(new EchoController(), EchoRequest.class,EchoResponse.class);
-	}
-	
-	public static class EchoController implements LambdaController<EchoRequest, EchoResponse>{
-		
-	}
+    public EchoHandler() {
+        super(new EchoController(), EchoRequest.class, EchoResponse.class);
+    }
+
+    public static class EchoController implements LambdaController<EchoRequest, EchoResponse> {
+        @RequestHandlerMethod
+        public EchoResponse handleRequest() {
+            return new EchoResponse("empty");
+        }
+    }
 }
--- test-project/src/main/java/handler/echo/EchoResponse.java
+++ test-project/src/main/java/handler/echo/EchoResponse.java
@@ -1,5 +1,13 @@
 package handler.echo;
 
-public class EchoResponse{
-	
+public class EchoResponse {
+    private final String message;
+
+    public EchoResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
