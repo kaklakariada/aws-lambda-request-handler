0c6adec03c10989bf6f660a8ebb719c91e577191
kaklakariada <kaklakariada@users.noreply.github.com>
2017-05-14 16:34:37 +0200

    Add junit 5

---
+++ src/test/java/com/github/kaklakariada/aws/lambda/RequestHandlingServiceTest.java
@@ -0,0 +1,17 @@
+package com.github.kaklakariada.aws.lambda;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class RequestHandlingServiceTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, 2);
+	}
+
+}
